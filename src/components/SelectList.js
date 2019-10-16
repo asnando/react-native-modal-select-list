7e05ab84d45d575dd70cb244726006f3722c0cf0
never00miss <allan.putra@yahoo.com>
2019-10-17 00:49:06 +0700

    Update SelectList.js

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -1,243 +1,85 @@
 import React, { PureComponent } from 'react';
-import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
-import optionsDefaultProps from '../constants/optionsDefaultProps';
-import optionPropTypes from '../constants/optionsPropTypes';
+import optionsDefaultProps from  '../constants/optionsDefaultProps';
+import optionsPropTypes from '../constants/optionsPropTypes';
 import {
-  SelectListContentContainer,
-  SelectListActivityIndicator,
-} from './SelectListContent.styles';
-import SelectListRow from './SelectListRow';
+  SelectListContainer,
+} from './SelectList.styles';
+import SelectListHeader from './SelectListHeader';
+import SelectListContent from './SelectListContent';
 
-const initialState = {
-  page: 1,
-  loading: true,
-  canLoadMoreOptions: true,
-  options: [],
-  filteredOptions: []
-};
-
-const filterOptionsListByText = (text, options) => {
-  const rgxpFilterByText = new RegExp(`^.*?(${text}).*?$`, 'i');
-  let optionFiltered = [];
-
-  options.map((option, key) => {
-    if(rgxpFilterByText.test(option.label)) {
-      optionFiltered.push(option)
-    } 
-    return option;
-  });
-
-  return optionFiltered
-};
-
-const mapVisiblePropertyToOptions = options => options.map((option) => {
-  if (typeof option.visible === 'undefined') {
-    option.visible = true;
-  }
-  return option;
-});
-
-class SelectListContent extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = initialState;
+class SelectList extends PureComponent {
+  handleHeaderInputChangeText(value) {
+    this.content.onHeaderInputChangeText(value);
   }
 
-  componentDidMount() {
-    this.resolveOptions();
+  saveContentComponentRef(ref) {
+    this.content = ref;
   }
 
-  // Will be called from the parent component when
-  // the modal header input change the text value.
-  onHeaderInputChangeText(text) {
-    const optionsProviderType = this.whichOptionsProviderType();
-    if (optionsProviderType === 'static') {
-      return this.filterOptionsListByText(text);
-    }
-    if (optionsProviderType === 'provider') {
-      return this.reset(this.getOptionsFromProvider.bind(this, text));
-    }
-    return null;
+  modalWillHide() {
+    const { content: contentComponent } = this;
+    return contentComponent.modalWillHide();
   }
 
-  getOptionsFromStaticList() {
-    const { options } = this.props;
-    return this.setOptionsList(options, this.setLoadingStatus.bind(this, false));
-  }
-
-  getOptionsFromProvider(text) {
-    const { page } = this.state;
-
+  render() {
     const {
+      placeholder,
+      closeButtonText,
+      onCloseModalRequest,
+      onRowSelected,
+      options,
       provider,
       pageSize,
       inputName,
       filter,
+      disableTextSearch,
+      headerTintColor,
+      buttonTextColor,
     } = this.props;
-
-    let providerOptions = {
-      page,
-      pageSize,
-      [inputName]: text,
-    };
-
-    // Extends the object containing additional filter keys
-    // to pass down to the provider function.
-    if (filter !== null && typeof filter !== 'undefined') {
-      if (typeof filter === 'function') {
-        providerOptions = {
-          ...providerOptions,
-          ...filter(),
-        };
-      }
-      providerOptions = {
-        ...providerOptions,
-        ...filter,
-      };
-    }
-
-    const value = provider(providerOptions);
-
-    if (value && typeof value.then === 'function') {
-      return value.then((options) => {
-        return this.addOptionsToList(options, () => {
-          return this.setLoadingStatus(true, () => {
-            // If provider returned less data than expected,
-            // then disable the load of more options.
-            if (options.length < pageSize) {
-              this.setState({ canLoadMoreOptions: false });
-            }
-          });
-        });
-      });
-    }
-    return this.addOptionsToList(value, this.setLoadingStatus.bind(this, false));
-  }
-
-  setLoadingStatus(status, callback) {
-    return this.setState({
-      loading: status,
-    }, callback);
-  }
-
-  setOptionsList(options, callback) {
-    return this.setState({
-      options: mapVisiblePropertyToOptions(options),
-    }, callback);
-  }
-
-  // We need to reset the static options visibility before every
-  // modal hide event. Otherwise, the last selected option
-  // from the list will be the only displayed in the list.
-  modalWillHide() {
-    const optionsProviderType = this.whichOptionsProviderType();
-    if (optionsProviderType === 'static') {
-      this.resetOptionsListVisibility();
-    }
-  }
-
-  resetOptionsListVisibility() {
-    const { options } = this.props;
-
-    return this.setOptionsList(options);
-  }
-
-  addOptionsToList(options, callback) {
-    const { options: prevStateOptions } = this.state;
-    return this.setState({
-      options: mapVisiblePropertyToOptions(prevStateOptions.concat(options)),
-    }, callback);
-  }
-
-  reset(callback) {
-    return this.setState(initialState, callback);
-  }
-
-  whichOptionsProviderType() {
-    const { provider } = this.props;
-    if (typeof provider === 'function') {
-      return 'provider';
-    }
-    return 'static';
-  }
-
-  filterOptionsListByText(text) {
-    const { options } = this.state;
-
-    if(text.length == 0) {
-      return this.setOptionsList(this.props.options);
-    }
-
-    return this.setOptionsList(filterOptionsListByText(text, options));
-  }
-
-  resolveOptions() {
-    const optionsProviderType = this.whichOptionsProviderType();
-    switch (optionsProviderType) {
-      case 'static':
-        return this.getOptionsFromStaticList();
-      case 'provider':
-        return this.getOptionsFromProvider();
-      default:
-        break;
-    }
-    return null;
-  }
-
-  handleEndListReached() {
-    const optionsProviderType = this.whichOptionsProviderType();
-    if (optionsProviderType === 'provider') {
-      this.requestNextPage();
-    }
-  }
-
-  requestNextPage() {
-    const { page, loading, canLoadMoreOptions } = this.state;
-    // Abort if already loading
-    if (loading || !canLoadMoreOptions) return;
-    this.setState({
-      loading: true,
-      page: page + 1,
-    }, this.getOptionsFromProvider.bind(this));
-  }
-
-  renderRow({ item }) {
-    const { onRowSelected } = this.props;
-    return item.visible && (<SelectListRow {...item} onRowSelected={onRowSelected} />);
-  }
-
-  renderFooter() {
-    const { loading } = this.state;
-    return loading && (
-      <SelectListActivityIndicator size="large" />
-    );
-  }
-
-  render() {
-    const { options } = this.state;
     return (
-      <SelectListContentContainer>
-        <FlatList
-          data={options}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={(...args) => this.renderRow(...args)}
-          ListFooterComponent={() => this.renderFooter()}
-          onEndReached={() => this.handleEndListReached()}
-          onEndReachedThreshold={1}
+      <SelectListContainer>
+        <SelectListHeader
+          placeholder={placeholder}
+          closeButtonText={closeButtonText}
+          disableTextSearch={disableTextSearch}
+          onCloseModalRequest={onCloseModalRequest}
+          onHeaderInputChangeText={(...args) => this.handleHeaderInputChangeText(...args)}
+          headerTintColor={headerTintColor}
+          buttonTextColor={buttonTextColor}
         />
-      </SelectListContentContainer>
+        <SelectListContent
+          options={options}
+          provider={provider}
+          pageSize={pageSize}
+          inputName={inputName}
+          filter={filter}
+          onRowSelected={onRowSelected}
+          ref={(...args) => this.saveContentComponentRef(...args)}
+        />
+      </SelectListContainer>
     );
   }
 }
 
-SelectListContent.defaultProps = {
+SelectList.defaultProps = {
+  placeholder: null,
+  closeButtonText: null,
+  disableTextSearch: false,
+  headerTintColor: null,
+  buttonTextColor: null,
   ...optionsDefaultProps,
 };
 
-SelectListContent.propTypes = {
-  ...optionPropTypes,
+SelectList.propTypes = {
+  placeholder: PropTypes.string,
+  closeButtonText: PropTypes.string,
+  onCloseModalRequest: PropTypes.func.isRequired,
   onRowSelected: PropTypes.func.isRequired,
+  disableTextSearch: PropTypes.bool,
+  headerTintColor: PropTypes.string,
+  buttonTextColor: PropTypes.string,
+  ...optionsPropTypes,
 };
 
-export default SelectListContent;
+export default SelectList;
